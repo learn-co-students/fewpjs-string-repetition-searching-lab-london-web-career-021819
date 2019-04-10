4ced32e865b51acc2643ba7e236ec7b927e6bbc5
Alex Bujenita <dekc89@gmail.com>
2019-04-10 11:20:25 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution here
+const nana = "Na na na na na na na, na na na na, hey Jude."
+const heyJude = _ => nana.repeat(16)
+
+const re = /candycorn|brusselssprouts/
+const prohibitedLanguage = word => word.search(re)
