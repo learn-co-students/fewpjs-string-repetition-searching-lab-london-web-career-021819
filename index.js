e3d15439179e8759405fbb3d6b5c728c65965273
CalieR <calielrushton@gmail.com>
2019-04-13 16:50:20 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+function heyJude() {
+  return "Na na na na na na na, na na na na, hey Jude.".repeat(16);
+}
+
+function prohibitedLanguage(username) {
+  return username.search(/candycorn|brusselssprouts/);
+}
