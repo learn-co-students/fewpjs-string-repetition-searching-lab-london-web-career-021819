378c598baf9338ad09b3145967d1f871b4f90904
byronsk <vyskoutaris@hotmail.com>
2019-04-13 17:04:08 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+
+function heyJude () {
+  let heyJude = 'Na na na na na na na, na na na na, hey Jude.'
+  return heyJude.repeat(16)
+}
+
+function prohibitedLanguage (str) {
+  let badNames = /candycorn|brusselssprouts/
+  return str.search(badNames)
+}
