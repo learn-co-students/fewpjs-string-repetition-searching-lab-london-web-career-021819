2859975174899e350756472095765f0f0e178e3d
louieogrady <l.ogrady@gmail.com>
2019-04-13 11:02:28 +0100

    fin

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+
+function heyJude() {
+  return "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+}
+
+function prohibitedLanguage(string) {
+  let bannedWords = /brusselssprouts|candycorn/
+  return string.search(bannedWords)
+}
