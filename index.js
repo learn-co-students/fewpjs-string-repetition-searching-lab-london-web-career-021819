f2a93b65890439d53b573a4cad8bc4441faff639
myrtokonst <myrtokonst@gmail.com>
2019-04-30 09:41:29 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+let verse = "Na na na na na na na, na na na na, hey Jude."
+
+let heyJude = () => verse.repeat(16)
+
+let prohibitedLanguage = word =>
+  word.search(/candycorn|brusselssprouts/)
