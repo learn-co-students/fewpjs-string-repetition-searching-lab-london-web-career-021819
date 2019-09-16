c3009deaf7e9945da5f06b7d0418ec6df61e562c
schazbot <charlotte.pearce1984@gmail.com>
2019-09-16 11:27:54 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function heyJude() { 
+   return "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+}
+
+function prohibitedLanguage(string) { 
+    let swears = /candycorn|brusselssprouts/
+    return string.search(swears)
+}
